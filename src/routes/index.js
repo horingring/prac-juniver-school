7b8d5595f3830d466cc74a346fe525150c7df173
호링링\dldld <dldldksl@naver.com>
2024-02-15 16:53:18 +0900

    [add] router_커리큘럼, 워크북 path 접근시 default query params 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,41 @@ import CurriculumPage from '@/pages/CurriculumPage.vue';
 import WorkBookPage from '@/pages/WorkBookPage.vue';
 
 const routes = [
-  { path: '/', component: IntroPage },
-  { path: '/curriculum', component: CurriculumPage },
-  { path: '/workbook', component: WorkBookPage },
+  {
+    path: '/',
+    name: 'Intro',
+    component: IntroPage,
+  },
+  {
+    path: '/curriculum',
+    name: 'Curriculum',
+    component: CurriculumPage,
+    beforeEnter: (to, from, next) => {
+      if (!Object.prototype.hasOwnProperty.call(to.query, 't')) {
+        next({
+          path: '/curriculum',
+          query: { t: 'math' },
+        });
+      } else {
+        next();
+      }
+    },
+  },
+  {
+    path: '/workbook',
+    name: 'Workbook',
+    component: WorkBookPage,
+    beforeEnter: (to, from, next) => {
+      if (!Object.prototype.hasOwnProperty.call(to.query, 't')) {
+        next({
+          path: '/workbook',
+          query: { t: 'level1' },
+        });
+      } else {
+        next();
+      }
+    },
+  },
 ];
 
 const router = createRouter({

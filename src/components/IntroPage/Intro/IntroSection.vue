0216634f06f209d9d785ab897a0578654794be71
호링링\dldld <dldldksl@naver.com>
2024-02-23 20:10:16 +0900

    [add & config] lodash 추가 & util함수 추가/설정 & header show/hide 추가

--- src/components/IntroPage/Intro/IntroSection.vue
+++ src/components/IntroPage/Intro/IntroSection.vue
@@ -4,7 +4,7 @@
     <div class="introSec__inner">
       <IntroDescription />
       <IntroDownBtnBox />
-      <button class="introSec__toDownBtn"></button>
+      <button class="introSec__toDownBtn" @click="scrollToRcmdSection"></button>
     </div>
   </section>
 </template>
@@ -13,11 +13,20 @@
 import IntroBgSlide from './IntroBgSlide.vue';
 import IntroDescription from './IntroDescription.vue';
 import IntroDownBtnBox from './IntroDownBtnBox.vue';
+import { getAbsoluteOffset } from '@/utils/getElementOffset';
+
 export default {
   components: {
     IntroBgSlide,
     IntroDescription,
     IntroDownBtnBox,
+  },
+  methods: {
+    scrollToRcmdSection() {
+      const rcmdSec = document.querySelector('.rcmdSec');
+      const { top } = getAbsoluteOffset(rcmdSec);
+      window.scrollTo(0, top);
+    },
   },
 };
 </script>
--- src/components/gnb/GnbHeader.vue
+++ src/components/gnb/GnbHeader.vue
@@ -2,6 +2,7 @@
   <header
     class="header flex-center"
     :class="{
+      'header--hide': !show,
       'header--yellow': urlQuery.t == 'level1',
       'header--skyblue': urlQuery.t == 'level2',
     }"
@@ -16,15 +17,43 @@
 <script>
 import GnbLogo from './GnbLogo.vue';
 import GnbList from './GnbList.vue';
+import { debounce } from 'lodash';
 
 export default {
   components: {
     GnbLogo,
     GnbList,
   },
+  data() {
+    return {
+      show: true,
+      lastScrollTop: 0,
+    };
+  },
   computed: {
     urlQuery() {
       return this.$route.query;
+    },
+  },
+  created() {
+    this.debouncedOnScroll = debounce(this.onScroll, 50);
+    window.addEventListener('scroll', this.debouncedOnScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.debouncedOnScroll);
+  },
+  methods: {
+    onScroll() {
+      // 스크롤 down
+      const currentScroll = window.scrollY;
+      if (currentScroll > this.lastScrollTop) {
+        this.show = false;
+      }
+      // 스크롤 up
+      else {
+        this.show = true;
+      }
+      this.lastScrollTop = currentScroll;
     },
   },
 };

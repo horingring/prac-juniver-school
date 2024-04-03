19cec56b4dddcc208219e12cbd2ed358abacc81a
호링링\dldld <dldldksl@naver.com>
2024-04-03 15:37:41 +0900

    [add] CurriculumPage__LogicPage__헤더 퍼블 추가

--- src/pages/curriculum/CurriculumPage.vue
+++ src/pages/curriculum/CurriculumPage.vue
@@ -2,6 +2,7 @@
   <main id="curriculumPage">
     <MathPage v-if="urlQuery.t === 'math'" />
     <CodingPage v-else-if="urlQuery.t === 'coding'" />
+    <LogicPage v-else-if="urlQuery.t === 'logic'" />
     <CurriculumFooter :subject="urlQuery.t" />
   </main>
 </template>
@@ -9,12 +10,14 @@
 <script>
 import MathPage from './MathPage.vue';
 import CodingPage from './CodingPage.vue';
+import LogicPage from './LogicPage.vue';
 import CurriculumFooter from '@/components/CurriculumPage/common/CurriculumFooter.vue';
 
 export default {
   components: {
     MathPage,
     CodingPage,
+    LogicPage,
     CurriculumFooter,
   },
   computed: {
---
+++ src/pages/curriculum/LogicPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="logicPage curriculumPage">
+    <IntroSection useBadge>
+      <template #heading>
+        인지 자극 쑥쑥!<br />
+        <StrongSpan underline underlineColor="#8d7fe4"
+          >사고력과 문제 해결력</StrongSpan
+        >을 길러요
+      </template>
+      <template #badge>
+        <p class="introSec__titleBadgeText">
+          서울대학교 아동가족학과<br />
+          박유정 교수 연구팀
+        </p>
+      </template>
+    </IntroSection>
+  </div>
+</template>
+
+<script>
+import IntroSection from '@/components/CurriculumPage/common/IntroSection.vue';
+import StrongSpan from '@/components/common/ui/StrongSpan.vue';
+
+export default {
+  components: {
+    IntroSection,
+    StrongSpan,
+  },
+};
+</script>
+
+<style></style>

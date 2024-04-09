0238c5321f244652f9a4f4ecd3dc33ff7ed9cfa1
호링링\dldld <dldldksl@naver.com>
2024-04-09 16:03:33 +0900

    [add] CurriculumPage__LabPage__CurriculumSectionWrap__데이터 수정 및 퍼블 완료

--- src/components/CurriculumPage/labPage/CurriculumSectionWrap.vue
+++ src/components/CurriculumPage/labPage/CurriculumSectionWrap.vue
@@ -9,9 +9,7 @@
       <h3 class="crclmSec__title">
         <StrongSpan class="crclmSec__titleMain"> {{ crclm.title }} </StrongSpan>
       </h3>
-      <p class="crclmSec__titleText">
-        {{ crclm.titleText }}
-      </p>
+      <p class="crclmSec__titleText" v-html="crclm.titleText"></p>
       <ul class="crclmSec__crclmList flex-center">
         <li
           v-for="card in crclm.cards"
@@ -39,123 +37,97 @@ export default {
       crclmList: [
         {
           id: 0,
-          name: 'discrimination',
-          title: '변별',
-          titleSub: '이 그림과 같은 것은 무엇일까?',
-          titleText: '두 개 이상의 그림을 보고 서로 같고 다름을 구별해요',
+          name: 'machine',
+          title: '기계와 컴퓨터',
+          titleText:
+            '<strong class="underline">도르래・컴퓨터・전기 회로</strong> 등을 직접 조립하고, 코딩으로 움직여요',
           cards: [
             {
               id: 0,
-              descYn: 'Y',
-              desc: '색, 모양, 위치, 방향이<br />같은 것을 찾아요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_01.png'),
-              caption: '빵이 먹고 싶어요',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_01.png'),
+              caption: '수레를 들어 올리자',
             },
             {
               id: 1,
-              descYn: 'Y',
-              desc: '부분을 보고<br />전체 그림을 찾아요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_02.png'),
-              caption: '꼭꼭 숨어라, 무늬 보일라',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_02.png'),
+              caption: '컴퓨터를 고치자',
             },
             {
               id: 2,
-              descYn: 'Y',
-              desc: '전체 그림의<br />부분 조각을 찾아요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_03.png'),
-              caption: '뚝딱뚝딱 공사장',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_03.png'),
+              caption: '조명을 고치자',
             },
           ],
         },
         {
           id: 1,
-          name: 'memory',
-          title: '기억',
-          titleSub: '내가 본 것은 무엇일까?',
-          titleText: '보았던 것을 기억하며 집중력을 길러요',
+          name: 'ai',
+          title: '인공지능과 데이터',
+          titleText:
+            '<strong class="underline">동영상 추천 알고리즘, 머신 러닝, 챗봇</strong> 등의 원리에 대해 이해하고,<br />관련 데이터를 시각화해요',
           cards: [
             {
               id: 0,
-              descYn: 'Y',
-              desc: '내가 본 것을 기억해요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_04.png'),
-              caption: '냠냠 피자 놀이',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_04.png'),
+              caption: '정답은 바로, 데이터!',
             },
             {
               id: 1,
-              descYn: 'Y',
-              desc: '색깔의 순서를 기억해요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_05.png'),
-              caption: '알록달록 드럼을 쳐요',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_05.png'),
+              caption: '팜팜이에게 가르쳐줘!',
             },
             {
               id: 2,
-              descYn: 'Y',
-              desc: '위치나 장면을 기억해요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_06.png'),
-              caption: '버스 탄 순서를 외워보자',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_06.png'),
+              caption: '챗봇을 만들어줘',
             },
           ],
         },
         {
           id: 2,
-          name: 'grouping',
-          title: '범주화',
-          titleSub: '어떻게 묶을 수 있을까?',
+          name: 'environment',
+          title: '환경과 에너지',
           titleText:
-            '서로 다른 생김새의 물건을 같은 기준으로 묶어 그룹화하는 능력을 길러요',
+            '바다쓰레기, 소똥 등 환경 오염의 다양한 원인들을<br /><strong class="underline">3D 프린터, 머신 러닝</strong> 등의 기술을 이용해서 해결해요',
           cards: [
             {
               id: 0,
-              descYn: 'N',
-              desc: '',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_07.png'),
-              caption: '동물 친구를 찾아라',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_07.png'),
+              caption: '산호의 집을 만들자',
             },
             {
               id: 1,
-              descYn: 'N',
-              desc: '',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_08.png'),
-              caption: '탈것을 찾아라',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_08.png'),
+              caption: '쓰레기를 분리해줘',
             },
             {
               id: 2,
-              descYn: 'N',
-              desc: '',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_09.png'),
-              caption: '오늘은 빨래하는 날',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_09.png'),
+              caption: '황제펭귄의 알을 지켜줘',
             },
           ],
         },
         {
           id: 3,
           name: 'space',
-          title: '공간지각',
-          titleSub: '어떤 조각을 넣어야 완성될까?',
+          title: '우주와 항공',
           titleText:
-            '사물과 공간의 위치, 방향 관계를 파악해 공간 감각력을 길러요',
+            '<strong class="underline">우주의 행성들과 달</strong>에 대해 알아보고, 코딩으로 문제를 해결해요',
           cards: [
             {
               id: 0,
-              descYn: 'Y',
-              desc: '두 조각을 합치면<br />어떤 모습인지 찾아요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_10.png'),
-              caption: '종이 조각을 합치면?',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_10.png'),
+              caption: '화성 탐사선을 찾아줘',
             },
             {
               id: 1,
-              descYn: 'Y',
-              desc: '도형을 채워<br />빠진 길을 완성해요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_11.png'),
-              caption: '고양이를 도와줘',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_11.png'),
+              caption: '우주 쓰레기를 청소하자',
             },
             {
               id: 2,
-              descYn: 'Y',
-              desc: '회전했을 때<br />같은 그림을 찾아요',
-              img: require('@/assets/img/CurriculumPage/logicPage/img_card_logic_12.png'),
-              caption: '웰컴 투 고스트 월드',
+              img: require('@/assets/img/CurriculumPage/labPage/img_card_lab_12.png'),
+              caption: '달에 기지를 보내줘',
             },
           ],
         },
